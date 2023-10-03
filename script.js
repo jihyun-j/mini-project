41b68a9a0412f40233ca1616f55a8435452b8fee
jihyun-j <jihyun9142@gmail.com>
2023-10-04 00:46:57 +0900

    Delete comment

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
-// 방명록 폼
-const userNameInput = document.querySelector(".user-name");
-const passwordInput = document.querySelector(".password");
-const commentInput = document.querySelector(".comments");
-const submitBtn = document.querySelector(".form-submit");
-
-// 작성된 방명록
-const boardContainer = document.querySelector(".board-contents-section");
-
-// 비밀번호 확인 Modal
-const checkPasswordForm = document.querySelector(".check-password-form");
-const checkPasswordInput = document.querySelector(".delete-password-input");
-const cancelDeleteBtn = document.querySelector(".delete-cancel");
-const checkPasswordBtn = document.querySelector(".delete");
-
-// 내용 수정 Modal
-const editCommentsForm = document.querySelector(".edit-comments-form");
-const editCommentsInput = document.querySelector(".edit-comments");
-const cancelEditCommentsBtn = document.querySelector(".edit-cancel");
-const editCommentsBtn = document.querySelector(".edit");
-
-// 나의 소개
-const showMainCard1 = document.querySelector("#card-1");
-const showMainCard2 = document.querySelector("#card-2");
-const showMainCard3 = document.querySelector("#card-3");
-
-const mbti = document.querySelector(".hidden-mbti");
-const mbtiImg = document.querySelector(".mbti-img");
-
+// Firebase setting
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import {
   getFirestore,
   collection,
   addDoc,
   getDocs,
+  getDoc,
   deleteDoc,
   Timestamp,
   doc,
+  orderBy,
+  query,
 } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -50,8 +25,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const ref = collection(db, "miniProject");
 
-// ! 게시물 등록하기
+// ! 방명록 제출 event
+const userNameInput = document.querySelector(".user-name");
+const passwordInput = document.querySelector(".password");
+const commentInput = document.querySelector(".comments");
+const submitBtn = document.querySelector(".form-submit");
+
 submitBtn.addEventListener("click", async function (e) {
   // * Default 이벤트 제거
   e.preventDefault();
@@ -69,91 +50,140 @@ submitBtn.addEventListener("click", async function (e) {
     date: getDate.toDate(),
     password: getPassword,
   };
+
   await addDoc(collection(db, "miniProject"), doc);
 
   // * Reload browser
   window.location.reload();
 });
 
-// ! 게시물 데이터 가져오기
-let docs = await getDocs(collection(db, "miniProject"));
-docs.forEach((doc) => {
-  let data = doc.data();
-  let comments = data.comments;
-  let userName = data.userName;
-  let createdDate = data.date;
-  let password = data.password;
+// ! 작성된 방명록 출력하기
+const boardContainer = document.querySelector(".board-contents-section");
 
-  // * 새로운 element 요소 추가하기
-  const div = document.createElement("div");
-  const usernameData = document.createElement("p");
-  const commentsData = document.createElement("p");
-  const dateData = document.createElement("p");
-  const deleteBtn = document.createElement("div");
-  const editBtn = document.createElement("div");
-
-  // * element 요소에 class name 추가하기
-  div.classList.add("board-data-container");
-  usernameData.classList.add("user-name-data");
-  commentsData.classList.add("comments-data");
-  dateData.classList.add("created-date-data");
-  deleteBtn.classList.add("delete-btn");
-  editBtn.classList.add("edit-btn");
-
-  // * div container 안에 username과 comments 내용 넣기
-  boardContainer.append(div);
-  div.append(usernameData);
-  div.append(commentsData);
-  div.append(deleteBtn);
-  div.append(dateData);
-  div.append(editBtn);
-
-  // * html 요소 안에 텍스트 넣기
-  deleteBtn.innerHTML = `<span class="material-symbols-outlined">delete</span>`;
-  editBtn.innerHTML = `<span class="material-symbols-outlined">edit</span>`;
-  usernameData.innerText = `${userName}`;
-  commentsData.innerText = `${comments}`;
-  dateData.innerText = createdDate.toDate();
-
-  deleteBtn.addEventListener("click", function () {
-    // * delete 버튼 클릭 했을때 document id# 가져오기
-    const docId = doc.id;
-    // * 각 코멘트 delete 버튼에 modal 추가하기
-    deleteDocItem(docId);
-  });
-});
-
-// ! Document 삭제하기 function
-function deleteDocItem(id) {
-  // * document의 정확한 path 가져오기
-  const docRef = doc(db, "miniProject", id);
-
-  // * deleteDoc 함수 이용해서 document 삭제하기
-
-  deleteDoc(docRef)
-    .then(() => {
-      // * document 삭제 후 페이지 리로드 시켜서 데이터 업데이트 시키기
-      window.location.reload();
-      checkPasswordForm.classList.remove("modal");
-      console.log("deleted");
-    })
-    .catch((err) => {
-      console.log("error");
-    });
+async function getComments() {
+  // let docs = await getDocs(collection(db, "miniProject"));
+  const dataList = await getDocs(query(ref, orderBy("date", "desc")));
+  addComment(dataList);
 }
 
-// * mbti 이미지
-mbti.addEventListener("mouseover", function () {
-  mbtiImg.classList.remove("hidden");
-  mbti.classList.add("hidden");
+// ! 제출한 방명록 추가하기
+function addComment(docs) {
+  docs.forEach((doc) => {
+    let data = doc.data();
+    let comments = data.comments;
+    let userName = data.userName;
+    let createdDate = data.date;
+
+    // * 새로운 container element 추가하기
+    const div = document.createElement("div");
+
+    // * container element에 class name 추가하기
+    div.classList.add("board-data-container");
+
+    // * container에 들어갈 내용 template
+    div.innerHTML = ` 
+    <p class="user-name-data">${userName}</p>
+    <p class="comments-data">${comments}</p>
+    <p class="created-date-data">${createdDate}</p>
+    <button class="material-symbols-outlined delete-btn" value=${doc.id}>delete</button>
+    <button class="material-symbols-outlined edit-btn" value=${doc.id}>edit</button>
+    `;
+
+    // * container에 template 넣기
+    boardContainer.append(div);
+  });
+
+  deleteComments();
+  editComments();
+}
+
+// **************************** 수정 및 삭제 **************************** //
+const checkPasswordForm = document.querySelector(".check-password-form");
+const checkPasswordInput = document.querySelector(".delete-password-input");
+const cancelDeleteBtn = document.querySelector(".delete-cancel");
+const checkPasswordBtn = document.querySelector(".delete");
+const message = document.querySelector(".password-message");
+
+let currentCommentId = "";
+
+// card delete button에 event 추가하기
+function deleteComments() {
+  document.querySelectorAll(".delete-btn").forEach((button) =>
+    button.addEventListener("click", function (e) {
+      openModal(checkPasswordForm);
+      currentCommentId = e.target.value;
+    })
+  );
+}
+
+// modal cancel button에 event 추가하기
+cancelDeleteBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  closeModal(checkPasswordForm);
 });
 
-mbti.addEventListener("mouseleave", function () {
-  mbtiImg.classList.add("hidden");
-  mbti.classList.remove("hidden");
+// modal delete button에 event 추가하기
+checkPasswordBtn.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const getData = doc(db, "miniProject", currentCommentId);
+  const getDelDoc = await getDoc(getData);
+  const password = getDelDoc.data().password;
+  console.log(password);
+
+  if (checkPasswordInput.value === "") {
+    message.innerText = "비밀번호를 입력해주세요";
+    checkPasswordInput.value = "";
+  } else if (password !== checkPasswordInput.value) {
+    message.innerText = "비밀번호를 다시 입력해주세요";
+    checkPasswordInput.value = "";
+  } else if (password === checkPasswordInput.value) {
+    console.log("삭제되었습니다");
+    checkPasswordInput.value = "";
+    deleteDoc(getData)
+      .then(() => {
+        // * document 삭제 후 페이지 리로드 시켜서 데이터 업데이트 시키기
+        window.location.reload();
+        console.log("deleted");
+      })
+      .catch((err) => {
+        console.log("error");
+      });
+  }
 });
 
-function showMyPersonalities() {}
+// 내용 수정 Modal
+const editCommentsForm = document.querySelector(".edit-comments-form");
+const editCommentsInput = document.querySelector(".edit-comments");
+const cancelEditCommentsBtn = document.querySelector(".edit-cancel");
+const editCommentsBtn = document.querySelector(".edit");
 
-// * 소개 보여주기
-showMainCard1.addEventListener("click", showMyPersonalities);
+// card delete button에 event 추가하기
+function editComments() {
+  document.querySelectorAll(".edit-btn").forEach((button) =>
+    button.addEventListener("click", function (e) {
+      openModal(editCommentsForm);
+      currentCommentId = e.target.value;
+    })
+  );
+}
+
+// editCommentsBtn.addEventListener("click");
+
+// 나의 소개
+const showMainCard1 = document.querySelector("#card-1");
+const showMainCard2 = document.querySelector("#card-2");
+const showMainCard3 = document.querySelector("#card-3");
+
+const mbti = document.querySelector(".hidden-mbti");
+const mbtiImg = document.querySelector(".mbti-img");
+
+// ************************* Modal ************************* //
+function openModal(modal) {
+  modal.classList.remove("hidden");
+}
+
+function closeModal(modal) {
+  modal.classList.add("hidden");
+}
+
+getComments();
